2fdbb78a34fd696dead071a230101543636cfb9a
Ulises Bravo <38496517+ulisesbravo@users.noreply.github.com>
2018-09-19 15:46:35 -0300

    Atualização e Correção de Bugs

---
+++ web-instituto-libertat-0.3.2/rodape.php
@@ -0,0 +1,11 @@
+
+<div class="menu_rodape">
+	<?php include('menu.php'); ?>
+</div>
+<div class="logo_rodape">
+	<img src="img/logo-libertat2.png" alt="Logo Instituto Libertat" title="Instituto Libertat">
+
+<span>
+	<p>contato@institutolibertat.org<p>
+</span>
+</div>
