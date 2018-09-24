8fe0fc1b65056d46e3602c4e6237c138cb43da88
Ulises Bravo <38496517+ulisesbravo@users.noreply.github.com>
2018-09-24 16:44:16 -0300

    Atualização e Correção de Bugs

---
+++ web-instituto-libertat-0.3.3/rodape.php
@@ -0,0 +1,15 @@
+
+<div class="menu_rodape">
+	<li><a href="?pag=home">Home</a></li>
+	<li><a href="index.php#artigos">Artigos</a></li>
+	<li><a href="?pag=quemsomos">Quem Somos</a></li>
+	<!--<li><a href="?pag=institucional">Institucional</a>-->
+	<li><a href="?pag=linhaeditorial">Linha Editorial</a></li>
+</div>
+<div class="logo_rodape">
+	<img src="img/logo-libertat2.png" alt="Logo Instituto Libertat" title="Instituto Libertat">
+
+<span>
+	<p>contato@institutolibertat.org<p>
+</span>
+</div>
