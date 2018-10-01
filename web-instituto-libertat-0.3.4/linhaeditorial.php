cedac7bab057e1246328bdf887f123e52f1fa4dd
Ulises Bravo <38496517+ulisesbravo@users.noreply.github.com>
2018-10-01 15:04:02 -0300

    Atualizações e Correção de Bugs
    Projeto Integrador

---
+++ web-instituto-libertat-0.3.4/linhaeditorial.php
@@ -0,0 +1,29 @@
+<div class="info">
+    <h1>Linha Editorial</h1>
+</div>
+<div class="texto texto_linha info">
+	<div class="separa_bottom">
+		<h2>Valores</h2>
+		<p>Liberdade econômica e social, negação de autoritarismo social, econômico, político e ideológico (nulius in verba). Compromisso com a realidade factual, com a precisão e acurácia dos fatos. Seriedade. Negação de violência. Cordialidade, ponderação e educação.</p>
+	</div>
+	<div class="separa_bottom">
+		<h2>Paradigmas</h2>
+		<p>Valorização de estratégias positivas ao invés de negativas. Relacionamento de ideias com fatos reais. Releitura histórica sob a perspectiva da liberdade.</p>
+	</div>
+	<div class="separa_bottom">
+		<h2>Visão</h2>
+		<p>Ideias e apenas ideias podem iluminar a escuridão, e assim temos como visão a disseminação delas para que gradualmente as pessoas se concientizem de novas possibilidades de auto-organização.</p>
+	</div>
+	<div class="separa_bottom">
+		<h2>Termos</h2>
+		<p>Não usar termos raivosos e agressivos. Cuidado ao usar o termo "público" e dar preferência a termos mais adequados como "estatal". Prezar pelo correto uso da língua.</p>
+	</div>
+	<div class="separa_bottom">
+		<h2>Hierarquia de Temas</h2> 
+		<p>Estão no topo: Política e Economia seguido dos demais temas relacionados diretamente a liberdade como questões sociais e anarquia epistemológica.</p>
+	</div>
+	<div class="contato">
+		<h2>Contato</h2>
+		<p>Se você tem interesse em contribuir com artigos para o nosso site e está de acordo com as nossas diretrizes, envie o material para a nossa editora de conteúdo pelo e-mail <b>contato@institutolibertat.org</b></p>
+	</div>
+</div>
---
+++ web-instituto-libertat-0.3.4/quemsomos.php
@@ -0,0 +1,65 @@
+<div class="container">
+    <div class="principal">
+        <div class="info">
+            <h1>Quem Somos?</h1>
+        
+            <p class="texto">O Instituto Libertat é constituído de seus membros e de um conselho administrativo horizontal. A composição do conselho administrativo é dado por convite direto, ao passo que a aderência ao instituto como membro é dada de forma voluntária e espontânea.</p>
+        
+        </div>
+        <div class="conselho separa">
+            <h2 class="info">Conselho</h2>
+                        <table class="margem">
+                            <tbody>
+                                <div class="margem">
+                                <tr class="azul negrito">
+                                    <td><span>Finanças</span></td>
+                                    <td><span>Comunicação</span></td>
+                                    <td><span>Eventos</span></b></td>
+                                    <td><span>Conteúdo</span></td>
+                                    <td><span>Planejamento</span></td>
+                                    <td>Mídia</td>
+                                </tr>
+                                </div>
+                                <div>
+                                <tr>
+                                    <td>Felipe Sosnowski</td>
+                                    <td>Vitor Levi</td>
+                                    <td>Bruno Teixeira<br>
+                                        Matheus Aloysio</td>
+                                    <td>Carlo Requião</td>
+                                    <td>Alexis Efremides</td>
+                                    <td>Ulises Bravo</td>
+                                </tr>
+                                </div>
+                                <div>
+                                <tr>
+                                    <td><img src="http://www.institutolibertat.org/pics/Sosnowski.jpg"
+                                        alt="imagem de Felipe Sosnowski" title="Felipe Sosnowski"><br>
+                                    </td>
+                                    <td><br>
+                                        <img src="http://www.institutolibertat.org/pics/Levi.jpg"
+                                        alt="imagem de Vitor Levi" title="Vitor Levi"><br>
+                                        <br>
+                                    </td>
+                                    <td><img src="http://www.institutolibertat.org/pics/Teixeira.jpg"
+                                        alt="imagem de Bruno Teixeira" title="Bruno Teixeira"><br>
+                                        <img src="http://www.institutolibertat.org/pics/Aloysio.jpg"
+                                        alt="imagem de Matheus Aloysio" title="Matheus Aloysio"><br>
+                                    </td>
+                                    <td><a href="http://www.filosofisica.com" title="Prof. Carlo Requião da Cunha, Ph.D."
+                                        target="_blank"><img src="http://filosofisica.com/LIBER/fig/eu.jpg"
+                                            alt="imagem de Prof. Carlo Requião, Ph.D." title="Prof. Carlo Requião, Ph.D."
+                                            style="width: 58px; height: 105px;"></a><br>
+                                    </td>
+                                    <td><img src="http://www.institutolibertat.org/pics/Alexis.jpg"
+                                        alt="imagem de Alexis Efremides" title="Alexis Efremides" style="width: 114px; height: 75px;"><br>
+                                    </td>
+                                    <td><img src="http://www.institutolibertat.org/pics/Bravo.jpg"
+                                        alt="imagem de Ulises Bravo" title="Ulises Bravo" style="width: 73px; height: 98px;"></td>
+                                </tr>
+                                </div>
+                        </table>
+                  </td>
+            </div>
+    </div>
+</div>
