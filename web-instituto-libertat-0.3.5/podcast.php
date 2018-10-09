86a0100d54ecdb1545d850250034b08d751df740
Ulises Bravo <38496517+ulisesbravo@users.noreply.github.com>
2018-10-09 14:27:51 -0300

    Atualização e Correção de bugs
    Nova aba Podcast

---
+++ web-instituto-libertat-0.3.5/podcast.php
@@ -0,0 +1,30 @@
+<div class="container">
+    <div class="cont_podcast">
+		<div class="wrap_esquerda">
+				<span class="info">
+					<h1>Podcast</h1>
+				</span>
+				<div class="wrap_pod">
+					<div>
+						<img src="img/podcast/libercast1.png" title="LiberCast #1 - Apresentação com Prof. Carlo e Equipe" alt="Capa do Podcast"/>
+					</div>
+					<div class="wrap_texto">
+						<h4>Libercast #1 - Apresentação Instituto Libertat com Prof. Carlo e Equipe</h4>
+						<p>05/10/2018 por Prof. Carlo Requião</p>
+					</div>
+				</div>
+		</div><!-- fim da esquerda -->
+		<div class="wrap_direita info">
+			<h2>Top Podcast</h2>
+			<img id="topcast" src="img/podcast/libercast1.png"/>
+			<audio autoplay="autoplay" controls="controls">
+			<source src="audio/podcast/podcastlibertat1.ogg" type="audio/ogg"/>
+			seu navegador não suporta HTML5
+		</div><!-- fim da direita -->
+			
+			<!-- <div class="esquerda info">
+			<h2>Apoie nós</h2>-->
+			
+			<!--</div> FIM -->
+	</div><!-- FIM de principal-->
+</div><!-- FIM de container-->
