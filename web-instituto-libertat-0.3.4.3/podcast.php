fd97741e39acc29bc8655f05ddf8daf952f02dfc
Ulises Bravo <38496517+ulisesbravo@users.noreply.github.com>
2018-10-31 12:22:36 -0300

    Atualizações

---
+++ web-instituto-libertat-0.3.4.3/podcast.php
@@ -0,0 +1,43 @@
+<div class="container">
+    <div class="cont_podcast">
+		<div class="wrap_esquerda">
+				<span class="info">
+					<h1>Podcast</h1>
+				</span>
+				<div class="wrap_pod">
+					
+					<div>
+						<img src="img/podcast/libercast.png" title="Libercast #2 - Demo(nio)cracia" alt="Capa do Podcast"/>
+					</div>
+					<div class="wrap_texto">
+						<h4>Libercast #2 - Demo(nio)cracia</h4>
+						<p>05/10/2018 por Prof. Carlo Requião e Equipe Libertat</p>
+					</div>
+				</div>
+				<div class="wrap_pod">
+					
+					<div>
+						<img src="img/podcast/libercast1.png" title="LiberCast #1 - Apresentação com Prof. Carlo e Equipe" alt="Capa do Podcast"/>
+					</div>
+					<div class="wrap_texto">
+						<h4>Libercast #1 - Apresentação Instituto Libertat com Prof. Carlo e Equipe</h4>
+						<p>05/10/2018 por Prof. Carlo Requião</p>
+					</div>
+				</div>
+		</div><!-- fim da esquerda -->
+		<div class="wrap_direita info">
+			<h2>Top Podcast</h2>
+			<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/519925023&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
+			<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/511471326&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
+			<!--<img id="topcast" src="img/podcast/libercast1.png"/>
+			<audio autoplay="autoplay" controls="controls">
+			<source src="audio/podcast/podcastlibertat1.ogg" type="audio/ogg"/>
+			seu navegador não suporta HTML5-->
+		</div><!-- fim da direita -->
+			
+			<!-- <div class="esquerda info">
+			<h2>Apoie nós</h2>-->
+			
+			<!--</div> FIM -->
+	</div><!-- FIM de principal-->
+</div><!-- FIM de container-->
