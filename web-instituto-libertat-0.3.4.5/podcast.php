a9078a81ab8ff3cd05e412f2a93a60c44e6f987c
Ulises Bravo <38496517+ulisesbravo@users.noreply.github.com>
2018-12-05 17:15:54 -0200

    Atualizações

---
+++ web-instituto-libertat-0.3.4.5/podcast.php
@@ -0,0 +1,76 @@
+<head>
+		<meta charset="UTF-8" />
+		<meta name="author" content="Ulises Bravo @ulisesbravo_">
+		<meta name="keywords" content="Instituto Libertat, Instituto Liberal, Paulo Guedes, Liber, Liberdade, Cultura Libertaria, Instituto, Libercast, Podcast Liberal, Podcast, Liber Podcast, Ideias Radicais">
+		<title>Instituto Libertat - Podcast</title>
+</head>
+<div class="container">
+    <div class="cont_podcast">
+		<div class="wrap_esquerda">
+				<span class="info">
+					<h1>Podcast</h1>
+				</span>
+				<div class="wrap_pod">
+					
+					<div class="wrap_icon_podcast">
+						<img src="img/podcast/libercast.png" title="Podcast Libercast #3 - Bens Publicos" alt="Capa do Podcast"/>
+					</div>
+					<div class="wrap_texto">
+						<h4>Libercast - EP #3 - Bens Publicos?</h4>
+						<p>23/11/2018 por Equipe Libertat com Azenha</p>
+						<div class="icones">
+							<a href=https://castbox.fm/episode/Libercast-EP-3-Bens-Publicos%3F-Podcast-id1452012-id101919556? target="_blank"><img src="img/redes/castbox_logo.png"></a>
+							<a href=https://open.spotify.com/episode/5zpiqNWSQSzIprWTZhizAl target="_blank"><img src="img/redes/Spotify-logo.png"></a>
+							<a href=https://soundcloud.com/institutolibertat/libercast-ep-3-bens-publicos-podcas target="_blank"><img id="soundcloud_icon" src="img/redes/soundcloud-logo.png"></a>
+						</div>
+						
+					</div>
+				</div>
+				<div class="wrap_pod">
+					
+					<div class="wrap_icon_podcast">
+						<img src="img/podcast/libercast.png" title="Podcast Libercast #2 - Demo(nio)cracia" alt="Capa do Podcast"/>
+					</div>
+					<div class="wrap_texto">
+						<h4>Libercast - EP #2 - Demo(nio)cracia</h4>
+						<p>15/10/2018 por Prof. Carlo Requião e Equipe Libertat</p>
+						<div class="icones">
+							<a href=https://castbox.fm/episode/Libercast-EP-2-Demo(nio)cracia--id1452012-id97456002? target="_blank"><img src="img/redes/castbox_logo.png"></a>
+							<a href=https://open.spotify.com/show/1f3gxeJU7jaZX29mVHMnB0?si=9Z9RBk4NR7iJnSYUpjPxFg target="_blank"><img src="img/redes/Spotify-logo.png"></a>
+							<a href=https://soundcloud.com/institutolibertat/libercast-ep-2-demoniocracia-1 target="_blank"><img id="soundcloud_icon" src="img/redes/soundcloud-logo.png"></a>
+						</div>
+					</div>
+				</div>
+				<div class="wrap_pod">
+					
+					<div class="wrap_icon_podcast">
+						<img src="img/podcast/libercast1.png" title="Podcast LiberCast #1 - Apresentação com Prof. Carlo e Equipe" alt="Capa do Podcast"/>
+					</div>
+					<div class="wrap_texto">
+						<h4>Libercast - EP #1 - Apresentação Instituto Libertat</h4>
+						<p>05/10/2018 por Prof. Carlo Requião e Equipe Libertat</p>
+						<div class="icones">
+							<a href=https://castbox.fm/episode/Libercast-1-Apresentação-Instituto-Libertat-com-Prof.-Carlo-id1452012-id95663694? target="_blank"><img src="img/redes/castbox_logo.png"></a>
+							<a href=https://open.spotify.com/episode/1nfUF3OfsqUAsrvHt4r2Ng target="_blank"><img src="img/redes/Spotify-logo.png"></a>
+							<a href=https://soundcloud.com/institutolibertat/podcast-apresentacao-instituto-libertat-com-prof-carlo target="_blank"><img id="soundcloud_icon" src="img/redes/soundcloud-logo.png"></a>
+						</div>
+					</div>
+				</div>
+		</div><!-- fim da esquerda -->
+		<div class="wrap_direita info">
+			<h2>Podcast Soundcloud</h2>
+			<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/534427977&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
+			<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/519925023&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
+			<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/511471326&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
+			<!--<img id="topcast" src="img/podcast/libercast1.png"/>
+			<audio autoplay="autoplay" controls="controls">
+			<source src="audio/podcast/podcastlibertat1.ogg" type="audio/ogg"/>
+			seu navegador não suporta HTML5-->
+		</div><!-- fim da direita -->
+			
+			<!-- <div class="esquerda info">
+			<h2>Apoie nós</h2>-->
+			
+			<!--</div> FIM -->
+	</div><!-- FIM de principal-->
+</div><!-- FIM de container-->
---
+++ web-instituto-libertat-0.3.4.5/rodape.php
@@ -0,0 +1,31 @@
+
+<div class="logo_rodape">
+	<img src="img/logo-libertat2.png" alt="Logo Instituto Libertat" title="Instituto Libertat">
+
+<span>
+	<p>contato@institutolibertat.org<p>
+</span>
+				
+</div>
+	<div>
+		<a href="https://www.instagram.com/institutolibertat/" target="_blank">
+			<img src="img/redes/icon-instagram.png" alt="instagram" title="Instituto Libertat no Instagram">
+		</a>
+		<a href="https://www.facebook.com/InsLibertat/" target="_blank">
+			<img src="img/redes/icon-facebook.png" alt="facebook" title="Instituto Libertat no Facebook">
+		</a>
+
+		<img src="img/redes/icon-youtube.png" alt="youtube" title="Instituto Libertat no Youtube">
+		<a href="https://www.twitter.com/InstLibertat/" target="_blank">
+			<img src="img/redes/icon-twitter.png" alt="twitter" title="Instituto Libertat no Twitter">
+		</a>
+	</div>
+<div class="menu_rodape">
+	<ul>
+		<li><a href="?pag=home">Home</a></li>
+		<li><a href="index.php#artigos">Artigos</a></li>
+		<li><a href="?pag=quemsomos">Quem Somos</a></li>
+		<li><a href="?pag=linhaeditorial">Linha Editorial</a></li>
+		<li><a href="?pag=podcast">Podcast</a></li>
+	</ul>
+</div>
