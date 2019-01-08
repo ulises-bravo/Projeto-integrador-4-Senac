11c4ac32cd15324bb1e9c9841c8137237722015b
Ulises Bravo <38496517+ulisesbravo@users.noreply.github.com>
2019-01-08 11:33:22 -0200

    Atualizações e Correções

---
+++ web-instituto-libertat-0.3.4.7/switch.php
@@ -0,0 +1,33 @@
+<?php
+error_reporting(0);
+
+switch ($_GET['pag']){
+	
+	case 'home': include "home.php";
+	break;
+	
+	case 'artigos': include "home.php";
+	break;
+	
+	case 'quemsomos': include "quemsomos.php";
+	break;
+	
+	case 'linhaeditorial': include "linhaeditorial.php";
+	break;
+	
+	case 'podcast': include "podcast.php";
+	break;
+	
+	case 'ajude': include "ajude.php";
+	break;
+	
+	default: include "home.php";
+	break;
+
+	case 'hess': include "artigos/hess.html";
+	break;
+
+	case 'filo1': include "artigos/filo1.html";
+	break;
+}
+?>
