08dd68875aece939f7e1244589e916b3e850e088
Ulises Bravo <38496517+ulisesbravo@users.noreply.github.com>
2019-03-26 16:29:22 -0300

    Atualização de páginas

---
+++ web-instituto-libertat-0.3.5.1/ajude.php
@@ -0,0 +1,18 @@
+<div class="container">
+    <div class="cont_podcast">
+		<div class="wrap_esquerda info">
+			<h2>DOAÇÕES</h2>
+			<p> Contribua com o Instituto Libertat e nossos projetos!</p>
+		
+		</div><!-- fim da esquerda -->
+		
+		<div class="wrap_direita info">
+			
+		</div><!-- fim da direita -->
+			
+			<!-- <div class="esquerda info">
+			<h2>Apoie nós</h2>-->
+			
+			<!--</div> FIM -->
+	</div><!-- FIM de principal-->
+</div><!-- FIM de container-->
